82973dd54c05c13a1cb25eba309e899b9963737c
SLotreck <lotrecks@msu.edu>
2021-09-14 16:28:13 -0400

    Add draft of code to utilize gene.py & add methods to Gene class

---
+++ data_preprocessing/methylation_binning/bin_methylation.py
@@ -0,0 +1,196 @@
+"""
+Calculate bin statistics for methylation data.
+
+Author: Serena G. Lotreck 
+"""
+from os.path import abspath
+import argparse 
+
+import pandas as pd 
+from collections import defaultdict
+
+def get_column_chunks(all_cols, chunksize):
+    """
+    Yield a generator that has a list of columns to include in a chunk.
+
+    parameters:
+        all_cols, list of str: list of column header names 
+        chunksize, int: number of columns to read in at a time
+    """
+    for i in range(0, len(all_cols), chunksize):
+        yield all_cols[i:i+chunksize]
+
+
+def read_methylation_file(filepath):
+    """
+    Read in a methylation file and transpose for more efficient handling.
+    Also splits the locus ID's into two, the seqid and the base pair number.
+
+    Assumes the methylation file is of the shape (n_accessions, n_base_pairs),
+    where the column headers are of the form 
+        <Chr#>_<bp#>_<methylation_type>_<+/->
+    and have no quoted commas.                          
+
+    parameters:
+        filepath, str: path to methylation file
+                                                                                        
+    returns: 
+        df, pandas df: methylation df where rows are base pairs and columns
+            are accession IDs
+    """
+    # Read in first row to get the column headers
+    print('\nReading file headers...')
+    with open(filepath) as f:
+        header_str = f.readline()
+    headers = header_str.split(',') 
+
+    # Read in chunks and concat to one df
+    print('\nReading in file...')
+    for i, use_cols in enumerate(get_column_chunks(headers, 100)):
+        print(f'Reading chunk {i} of {len(headers)%100}')
+        # If it's the first chunk, make this the df 
+        if i == 0:
+            # Read in 
+            df = pd.read_csv(filepath, use_cols=use_cols)
+            # Transpose 
+            df = df.T
+        # Otherwise, transpose and add to the bottom
+        else:
+            # Read in 
+            df_chunk = pd.read_csv(filepath, use_cols=use_cols)
+            # Transpose and append  
+            df = pd.concat([df, df_chunk.T])
+
+    # Split index into multiindex 
+    print('\nSplitting identifiers into chromosome ID and base pair...')
+    chr_idx = [df.index.str.split('_')[0]]
+    bp_idx = [df.index.str.split('_')[1]]
+    df.index = pd.MultiIndex.from_arrays([chr_idx, bp_idx])
+    df.rename_axis(("seqid", "bp"))
+
+    print(f'\nDone! Snapshot of dataframe: df.head()')
+    return df 
+
+
+def get_start_row(gff):
+    """
+    Determine at what row the data starts in a gff file.
+
+    parameters:
+        gff, str: path to gff file with genomic features
+    
+    returns:
+        start_row, int: the index of the row where the data starts, 
+            which is equivalent to the number of rows to skip
+    """
+    start_row = 0
+    with open(gff) as f:
+        line = f.readline()
+        line_num = 0 # Keep track of what line we're on 
+        while start_row == 0: # Only read lines until we find the start row 
+            if line[0] != '#': # If it doesn't start with a comment, it's the first data row 
+                start_row = line_num
+            else: line_num += 1
+
+    return start_row 
+
+
+def get_genes(gff):
+    """
+    Create Gene class instances for genomic features with the type "gene".
+
+    parameters:
+        gff, str: path to gff file with genomic features.
+
+    returns:
+        genes, dict of list: keys are seqid's (e.g. 'Chr1'), values are
+            lists of Gene instances with that seqid
+    """
+    # Determine at what row the data starts
+    start_row = get_start_row(gff)
+
+    # Read in the data 
+    gff_df = pd.read_csv(gff, sep='\t', skiprows=start_row,
+            names=['seqid', 'source', 'type', 'start', 'end', 'score', 
+                    'strand', 'phase', 'attributes'])
+
+    # Filter df to keep only 'gene' rows
+    genes_df = gff_df[gff_df['type'] == 'gene']
+
+    # Make gene objects 
+    genes = defaultdict(list)
+    for row in genes_df.itterrows():
+        gene = Gene(row['seqid'], row['start'], row['end'])
+        genes[row['seqid']].append(gene)
+
+    return genes 
+
+
+def main(gff, CG_pres_abs, CHG_pres_abs, CHH_pres_abs, CG_prop, CHG_prop,
+        CHH_prop, out_loc, file_prefix):
+
+    # Make gene objects 
+    genes = get_genes(gff)
+
+    # Read in methylation datasets 
+    methylation_datasets = {'CG_pres_abs' : CG_pres_abs, 
+                            'CHG_pres_abs': CHG_pres_abs, 
+                            'CHH_pres_abs': CHH_pres_abs, 
+                            'CG_prop'     : CG_prop, 
+                            'CHG_prop'    : CHG_prop,
+                            'CHH_prop'    : CHH_prop}
+    methylation_dfs = {}
+    for dataset_name, dataset in methylation_datasets.items():
+        print(f'\nReading in dataset {dataset_name}')
+        methylation_dfs[dataset_name] = read_methylation_file(dataset)
+
+    # Add methylation sites to Gene objects 
+    for seqid in genes.keys():
+        # Filter methylation datasets
+        current_seqid_dfs = {}
+        for dataset_name, df in methylation_dfs.items():
+            filtered_df = df.loc[dataset_name, :]
+            current_seqid_dfs[dataset_name] = filtered_df 
+        # Pass to Gene class methods
+        genes_to_check = genes[seqid]
+        for gene in genes_to_check:
+            for dataset_name, seqid_df in current_seqid_dfs.items():
+                gene.set_methylation_attr(seqid_dff, dataset_name)
+
+    # Do binning 
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description='Bin methylated sites')
+
+    parser.add_argument('-gff', type=str,
+            help='Path to gff3-formatted file with genomic features.')
+    parser.add-argument('-CG_pres_abs', type=str,
+            help='Path to file with presence/absence data for CG methylation.')
+    parser.add-argument('-CHG_pres_abs', type=str,
+            help='Path to file with presence/absence data for CHG methylation.')
+    parser.add-argument('-CHH_pres_abs', type=str,
+            help='Path to file with presence/absence data for CHH methylation.')
+    parser.add-argument('-CG_prop', type=str,
+            help='Path to file with proportion data for CG methylation.')
+    parser.add-argument('-CHG_prop', type=str,
+            help='Path to file with proportion data for CHG methylation.')
+    parser.add-argument('-CHH_prop', type=str,
+            help='Path to file with proportion data for CHH methylation.')
+    parser.add_argument('-out_loc', type=str,
+            help='Path to directory to save the output.')
+    parser.add_argument('-file_prefix', type=str,
+            help='String to prepend to output files.')
+
+    args = parser.parse_args()
+
+    args.gff = abspath(args.gff)
+    args.CG_pres_abs = abspath(args.CG_pres_abs)
+    args.CHG_pres_abs = abspath(args.CHG_pres_abs)
+    args.CHH_pres_abs = abspath(args.CHH_pres_abs)
+    args.CG_prop = abspath(args.CG_prop)
+    args.CHG_prop = abspath(args.CHG_prop)
+    args.CHH_prop = abspath(args.CHH_prop)
+    args.out_loc = abspath(args.out_loc)
+
+    main(**vars(args))
--- data_preprocessing/methylation_binning/gene.py
+++ data_preprocessing/methylation_binning/gene.py
@@ -6,22 +6,125 @@ Author: Serena G. Lotreck
 
 class Gene:
 
-    def __init__(self, gff_line):
+    def __init__(self, seqid, start, end):
         """
         Instantiate an instance of the Gene class. 
 
         parameters:
-            gff_line, list of str: line from a gff file split on \t that
-                represents the gene
+            seqid, str: ID from the first column of the gff file 
+            start, int: start bp
+            end, int: end bp
 
         returns: None
         """
-        self.seqid = gff_line[0]
-        self.start = int(gff_line[3])
-        self.end = int(gff_line[4])
+        self.seqid = seqid
+        self.start = start
+        self.end = end
 
-        self.CG = []
-        self.CHG = []
-        self.CHH = []
+        # These will be df's later 
+        self.CG_pres_abs = None
+        self.CHG_pres_abs = None
+        self.CHH_pres_abs = None
+        self.CG_prop = None
+        self.CHG_prop = None
+        self.CHH_prop = None
+        self.CG_pres_abs_mean = None
+        self.CHG_pres_abs_mean = None
+        self.CHH_pres_abs_mean = None
+        self.CG_prop_median = None
+        self.CHG_prop_median = None
+        self.CHH_prop_median = None
 
-    def 
+        
+    def set_methylation_attr(self, methylation_df, attr_name):
+        """
+        Filter methylation dataset by what sites are in this gene, 
+        or within 500bp up- or downstream, and add
+        the resulting df to the attribute corresponding to attr_name.
+
+        parameters:
+            methylation_df, df: index is the base pair at which the methylation
+                occurrs, column headers are accessions
+
+        returns: None 
+        """
+        # Check datatype of index & change to int if it's not int
+        if methylation_df.index.dtype != 'int64':
+            methylation_df.index = methylation_df.index.astype('int64')
+            
+        # Filter df by whether or not it's in the gene 
+        in_near_gene_df = methylation_df.loc[(self.start-500 < methylation_df.index) & 
+                                            (methylation_df.index < self.end+500)]
+
+        # Set attribute 
+        setttr(self, attr_name, in_near_gene_df)
+    
+
+    def set_bin_stat(self, methylation_type):
+        """
+        Separate sites into bins and calculate statistic across bins and 
+        accessions for a given type of methylation, and assign the result 
+        as an attribute.
+
+        NOTE: If there are no C's in a bin, there will be a NaN, as this 
+        seems to more accurately reflect what we're trying to demonstrate
+        rather than putting a 0 there. This may become a problem downstream,
+        which is why I mention it now. 
+
+        parameters:
+            methylation_type, str: name of methylation type to do this for.
+                Name should correspond to one of the methylation dataset 
+                attributes. 
+
+        returns: None 
+        """ 
+        # Get the dataset
+        df = getattr(self, methylation_type)
+
+        # Separate into in-gene and before/after-gene sites 
+        gene_body_df = df.loc[(self.start < df.index) & (df.index < self.end)]
+        upstream_df = df.loc[~(self.start < df.index)]
+        downstream_df = df.loc[~(df.index < self.end)]
+
+        # Bin and calculate within gene bins
+        
+        ## Within gene body 
+        min_bp = df.bp.min()
+        max_bp = df.bp.max()
+        num_gene_bins = 20
+        gene_body_df.bp = pd.cut(gene_body_df.bp, bins=np.arange(min_bp, max_bp, 
+                    step=(max_bp - min_bp)/num_gene_bins, include_lowest=True))
+        if 'pres_abs' in methylation_type:
+            gene_stat_df = gene_body_df.groupby('bp').mean()
+        elif 'prop' in methylation_type:
+            gene_stat_df = gene_body_df.groupby('bp').median()
+        # Rename bins ##TODO make this work 
+        bins = 
+        gene_bins = {bn:f'gene_body_bin{i}' for i, bn in enumerate(bins)}
+        gene_stat_df = gene_stat_df.T.rename(columns=gene_bins)
+
+
+        ## Upstream 
+        start_range = self.start - 500
+        num_up_bins = 5 # 100bp bins
+        upstream_df.bp = pd.cut(upstream_df.bp, bins=np.arange(start_range, 
+                    self.start, step=(self.start - start_range)/num_up_bins,
+                    include_lowest=True)) 
+        if 'pres_abs' in methylation_type:
+            up_stat_df = upstream_df.groupby('bp').mean()
+        elif 'prop' in methylation_type:
+            up_stat_df = upstream_df.groupby('bp').median()
+        # Rename bins
+        ##TODO
+        
+        ## Downstream 
+        end_range = self.end + 500
+        num_down_bins = 5 # 100bp bins
+        downstream_df.bp = pd.cut(downstream_df.bp, bins=np.arange(self.end, 
+                    end_range, step=(end_range - self.end)/num_down_bins,
+                    include_lowest=True)) 
+        if 'pres_abs' in methylation_type:
+            down_stat_df = downstream_df.groupby('bp').mean()
+        elif 'prop' in methylation_type:
+            down_stat_df = downstream_df.groupby('bp').median()
+        ##TODO
